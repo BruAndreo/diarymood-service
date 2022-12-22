137a570e66db61b5ea1d9409ab04af576b7d05d1
Bruno Lino <brunolino2026@gmail.com>
2022-12-21 21:36:39 -0300

    infra: criando objeto para centralizacao de env vars

---
+++ src/config/config.js
@@ -0,0 +1,11 @@
+
+export default {
+  PORT: process.env.SERVER_PORT,
+  DATABASE: {
+    DBNAME: process.env.DATABASE_NAME,
+    PORT: process.env.DATABASE_PORT,
+    HOST: process.env.DATABASE_HOST,
+    USER: process.env.DATABASE_USER,
+    PASSWORD: process.env.DATABASE_PASS
+  }
+};
