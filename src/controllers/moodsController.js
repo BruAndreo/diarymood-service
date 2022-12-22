b1bd45824d9e75f97076117811c58049c50be804
Bruno Lino <brunolino2026@gmail.com>
2022-12-21 22:13:30 -0300

    feat: inserindo novo mood no banco

--- src/controllers/moodsController.js
+++ src/controllers/moodsController.js
@@ -1,11 +1,33 @@
 import { StatusCodes } from "http-status-codes";
+import DBConnector from "../database/dbConnector.js";
+import MoodModel from "../model/moodModel.js";
 
 export default class MoodsController {
 
-  newMood(req, res) {
-    return res.status(StatusCodes.CREATED).json({
-      message: "Mood registered"
-    });
+  async newMood(req, res) {
+    try {
+      const body = req.body;
+
+      const conn = await DBConnector.getConnection();
+
+      const moodModel = MoodModel.load(conn);
+
+      const mood = new moodModel(body);
+      await mood.save();
+
+      DBConnector.close();
+
+      return res.status(StatusCodes.CREATED).json({
+        message: "Mood registered"
+      });
+    }
+    catch (e) {
+      console.error(`ERROR: ${e.message}`);
+
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        message: e.message
+      });
+    }
   }
 
 }
