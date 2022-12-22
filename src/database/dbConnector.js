d934010b3545607fd5f1c3425a2cc8c02b87aed2
Bruno Lino <brunolino2026@gmail.com>
2022-12-21 22:05:15 -0300

    infra: criando conexao com banco de dados

---
+++ src/database/dbConnector.js
@@ -0,0 +1,25 @@
+import { envVars } from "../config/config.js";
+import mongoose from "mongoose";
+
+export default class DBConnector {
+
+  static _getStringConnection() {
+    const { HOST, PORT, DBNAME } = envVars.DATABASE;
+
+    return `mongodb://${HOST}:${PORT}/${DBNAME}`;
+  }
+
+  static async getConnection() {
+    return await mongoose.connect(DBConnector._getStringConnection(), {
+        authSource: "admin",
+        user: envVars.DATABASE.USER,
+        pass: envVars.DATABASE.PASSWORD,
+        useNewUrlParser: true
+      });
+  }
+
+  static close() {
+    mongoose.connection.close();
+  }
+
+}
