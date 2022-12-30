0e88b456171343a2f837c04712987a107431766e
Bruno Lino <brunolino2026@gmail.com>
2022-12-30 09:32:48 -0300

    test: Adicionado teste para validacao de erro ao conectar com db

--- test/unit/service/moodService.test.js
+++ test/unit/service/moodService.test.js
@@ -4,6 +4,10 @@ import DBConnector from "../../../src/database/dbConnector.js";
 import MoodService from "../../../src/service/moodService.js";
 import MoodModel from "../../../src/model/moodModel.js";
 
+global.console = {
+  error: jest.fn()
+};
+
 describe("Mood Service", () => {
 
   it("Should connect with DB one time, and close one time", async () => {
@@ -23,6 +27,17 @@ describe("Mood Service", () => {
 
     expect(DBConnector.setUp).toHaveBeenCalledTimes(1);
     expect(DBConnector.close).toHaveBeenCalledTimes(1);
+  });
+
+  it("Should return error when DB Connection is failed", async () => {
+    jest.spyOn(DBConnector, 'setUp').mockRejectedValueOnce(new Error("Mocking error"));
+
+    const t = async () => {
+      const service = new MoodService();
+      await service.insertNewMood({});
+    }
+
+    expect(t).rejects.toThrow(Error);
   });
 
 });
